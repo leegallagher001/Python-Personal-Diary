cb0fec1d967b9663d8a0bdad6d80c1c04c4688d7
Lee Gallagher <lee.gallagher.writing@outlook.com>
2025-11-01 08:29:29 +0000

    Started work on "View Saved Entries" page
    The "View Saved Entries" page is now implemented onto the program. The page is built and designed, and the "View Saved Entries" button on the main menu successfully navigates to it, and the "Home" button on the new page successfully navigates back to home. The design looks as it should from what I wanted.
    
    Next steps, JSON is imported so will refactor the "submit" button to save the new entry there instead of as a .txt file, then it will be time to add a function that adds the entry to the "view saved" page as a clickable button that loads a page from which to read the entry.

--- ProtoDiaryGUI_V1.py
+++ ProtoDiaryGUI_V1.py
@@ -3,18 +3,22 @@
 # This is a first prototype of what the UI/UX of my Python Diary application may look like.
 # Initial goal is to simulate the "Main Menu" screen, however I will add more functionality where possible.
 
-# (0) Imports
+# (0.0) Imports
 
 from ast import Lambda
 import tkinter as tk
 from tkinter import *
+import json
 
-# (1) Window Configuration
+diary_entries = "pdgui-v1-entries.json"
+
+# (1.0) Window Configuration
 
 window = tk.Tk()
 
 home_page = tk.Frame(window, bg="lightgreen")
 new_entry_page = tk.Frame(window, bg="lightgreen")
+view_saved_entries_page = tk.Frame(window, bg="lightgreen")
 
 home_page.tkraise() # loads home page when program is opened
 
@@ -24,12 +28,21 @@ window.geometry("900x600")
 window.configure(bg="lightgreen")
 window.title("Python Diary Application GUI Prototype V1")
 
-# (2) Button Functions
+# (2.0) Button Functions
+
+# -- HOME PAGE -- #
 
 def add_new_entry_page(): # calls "new entry page" from home
     home_page.pack_forget()
     new_entry_page.pack(fill='both', expand=True)
     new_entry_page.tkraise()
+
+def access_current_entries_page():
+    home_page.pack_forget()
+    view_saved_entries_page.pack(fill='both', expand=True)
+    view_saved_entries_page.tkraise()
+
+# -- NEW ENTRY PAGE -- #
 
 def home_button_from_new(): # calls "home page" from "new entry page"
     new_entry_page.pack_forget()
@@ -59,8 +72,16 @@ def text_entry_to_txt():
     enter_date_textbox.delete("1.0", "end")
     enter_article_textbox.delete("1.0", "end")
 
+# -- VIEW SAVED ENTRIES PAGE
 
-# ---------- HOME PAGE ---------- #
+def home_button_from_saved(): # calls "home page" from "view saved entries page"
+    view_saved_entries_page.pack_forget()
+    home_page.pack(fill='both', expand=True)
+    home_page.tkraise()
+
+# (3.0) Page Layouts
+
+# ---------- Page 1 - HOME PAGE ---------- #
 
 # (1) Grid Configuration
 
@@ -72,13 +93,13 @@ for i in range(pg1rows):
 for i in range(pg1columns):
     home_page.grid_columnconfigure(i, weight=1)
 
-# (2) Main Menu
+# (2) Page Contents (Main Menu)
 
 title_label = tk.Label(home_page, text="-- PYTHON TEXT DIARY --", bg="green", fg="black", font=("Arial", 24)).grid(column=0, row=0, columnspan=2, sticky=NSEW, padx=5, pady=5)
 
 button_new = tk.Button(home_page, text="Add New Entry", borderwidth=2, relief="raised", fg="lightgreen", bg="black", font=("Arial", 20), command=add_new_entry_page).grid(column=0, row=1, rowspan=2, sticky=NSEW, padx=5, pady=5)
 
-button_view = tk.Button(home_page, text="View Saved Entries", borderwidth=2, relief="raised", fg="lightgreen", bg="black", font=("Arial", 20)).grid(column=1, row=1, rowspan=2, sticky=NSEW, padx=5, pady=5)
+button_view = tk.Button(home_page, text="View Saved Entries", borderwidth=2, relief="raised", fg="lightgreen", bg="black", font=("Arial", 20), command=access_current_entries_page).grid(column=1, row=1, rowspan=2, sticky=NSEW, padx=5, pady=5)
 
 button_delete = tk.Button(home_page, text="Delete Saved Entries", borderwidth=2, relief="raised", fg="lightgreen", bg="black", font=("Arial", 20)).grid(column=0, row=3, rowspan=2, sticky=NSEW, padx=5, pady=5)
 
@@ -86,7 +107,7 @@ button_help = tk.Button(home_page, text="Help & Readme Text", borderwidth=2, rel
 
 footer_label = tk.Label(home_page, text="Created by Lee Gallagher 2025", bg="green", fg="black", font=("Arial", 20)).grid(column=0, row=5, columnspan=2, sticky=NSEW, padx=5, pady=5)
 
-# ---------- NEW ENTRY PAGE ---------- #
+# ---------- Page 2 - NEW ENTRY PAGE ---------- #
 
 # (1) Grid Configuration
 
@@ -127,4 +148,29 @@ submit_button.grid(column=5, row=8, columnspan=5, sticky=NSEW, padx=5, pady=5)
 
 footer_label = tk.Label(new_entry_page, text="Created by Lee Gallagher 2025", bg="green", fg="black", font=("Arial", 20)).grid(column=0, row=9, columnspan=10, sticky=NSEW, padx=5, pady=5)
 
+# ---------- Page 3 - VIEW SAVED ENTRIES PAGE ---------- #
+
+# (1) Grid Configuration
+
+pg3rows = 10
+pg3columns = 10
+
+for i in range(pg3rows):
+    view_saved_entries_page.grid_rowconfigure(i, weight=1)
+for i in range(pg3columns):
+    view_saved_entries_page.grid_columnconfigure(i, weight=1)
+
+# (2) Page Contents
+
+title_label = tk.Label(view_saved_entries_page, text="-- PYTHON TEXT DIARY --", bg="green", fg="black", font=("Arial", 24)).grid(column=0, row=0, columnspan=10, sticky=NSEW, padx=5, pady=5)
+
+entries_container = tk.Frame(view_saved_entries_page, bg="green").grid(column=0, row=1, columnspan=10, rowspan=7, sticky=NSEW, padx=5, pady=5)
+
+home_button = tk.Button(view_saved_entries_page, text="HOME", bg="black", fg="lightgreen", font=("Arial", 16), command=home_button_from_saved).grid(column=0, row=8, columnspan=10, sticky=NSEW, padx=5, pady=5)
+
+footer_label = tk.Label(view_saved_entries_page, text="Created by Lee Gallagher 2025", bg="green", fg="black", font=("Arial", 20)).grid(column=0, row=9, columnspan=10, sticky=NSEW, padx=5, pady=5)
+
+# (4.0) Mainloop
+
 window.mainloop()
+
