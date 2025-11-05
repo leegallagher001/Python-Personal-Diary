2d1129d7e4f8cca6ae18789afa4081fa6f7a1378
Lee Gallagher <lee.gallagher.writing@outlook.com>
2025-11-05 01:18:18 +0000

    Created all pages of program, added functionality to "New Entry" and "View Saved Entries" pages
    Have been able to leverage my experience with PDGUI-V1 to rapidly build GUI-V2. All pages are built and the navigation buttons are linked, and the "New Entry" and "View Saved Entries" (and my extension the "Read Saved Entry") pages are all functional. 
    
    The "View Saved Entries" page now functions with the entries as buttons that lead directly to the "Read Entry" page - this is what I always wanted to do, but it took me a good deal of thought as to how to do it (it was a lack of knowledge of how to implement the "lambda" functions that tripped me up - but I've learned something through it).
    
    Overall, the code for GUI-V2 is much cleaner than GUI-V1. Very happy with it so far. Next step, get the "Delete" and "Help" pages up and running!

--- ProtoDiaryGUI_V2.py
+++ ProtoDiaryGUI_V2.py
@@ -7,11 +7,13 @@
 
 # (0) Imports
 
+from datetime import date
 import tkinter as tk
 from tkinter import *
 import json
+from turtle import title
 
-diary_entries = "pdgui-v2-entries.json"
+diary_entries_file = "pdgui-v2-entries.json"
 
 window = tk.Tk()
 window.geometry("900x600")
@@ -43,12 +45,12 @@ def new_entry(): # Page 2 - Add New Entry Page
 
     new_title_label = tk.Label(new_entry_page, bg="#245DDA", fg="white", font="Helvitica 18", text="enter title: ") # title section
     new_title_label.grid(row=1, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
-    new_entry_title = tk.Entry(new_entry_page, bg="lightblue", font="Helvitica 18")
+    new_entry_title = tk.Text(new_entry_page, bg="lightblue", font="Helvitica 18", width=45, height=1)
     new_entry_title.grid(row=1, column=2, columnspan=8, padx=5, pady=5, sticky="nsew")
 
     new_date_label = tk.Label(new_entry_page, bg="#245DDA", fg="white", font="Helvitica 18", text="enter date: ") # date section
     new_date_label.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
-    new_entry_date = tk.Entry(new_entry_page, bg="lightblue", font="Helvitica 18")
+    new_entry_date = tk.Text(new_entry_page, bg="lightblue", font="Helvitica 18", width=45, height=1)
     new_entry_date.grid(row=2, column=2, columnspan=8, padx=5, pady=5, sticky="nsew")
 
     new_main_label = tk.Label(new_entry_page, bg="#245DDA", fg="white", font="Helvitica 18", text="enter article: ") # main article section
@@ -56,10 +58,10 @@ def new_entry(): # Page 2 - Add New Entry Page
     new_entry_main = tk.Text(new_entry_page, bg="lightblue", font="Helvitica 18", width=45, height=10)
     new_entry_main.grid(row=3, rowspan=5, column=2, columnspan=8, padx=5, pady=5, sticky="nsew")
 
-    home_button = tk.Button(new_entry_page, bg="#245DDA", fg="white", font="Helvitica 18", relief=tk.RAISED, bd=5, text="home", command=lambda: home(new_entry_page)) # home button
+    home_button = tk.Button(new_entry_page, bg="#81C046", fg="white", font="Helvitica 18", relief=tk.RAISED, bd=5, text="home", command=lambda: home(new_entry_page)) # home button
     home_button.grid(row=8, column=0, columnspan=5, padx=5, pady=5, sticky="nsew")
 
-    submit_button = tk.Button(new_entry_page, bg="#81C046", fg="white", font="Helvitica 18", relief=tk.RAISED, bd=5, text="submit entry") # submit button
+    submit_button = tk.Button(new_entry_page, bg="#245DDA", fg="white", font="Helvitica 18", relief=tk.RAISED, bd=5, text="submit entry", command=lambda: save_new_entry(new_entry_title, new_entry_date, new_entry_main)) # submit button
     submit_button.grid(row=8, column=5, columnspan=5, padx=5, pady=5, sticky="nsew")
 
     # Footer
@@ -77,6 +79,7 @@ def view_entries(): # Page 3 - View Saved Entries Page
 
     # Grid Configuration
 
+    global view_entries_page
     view_entries_page = tk.Frame(window, bg="#81C046")
 
     pg3rows = 10
@@ -97,6 +100,14 @@ def view_entries(): # Page 3 - View Saved Entries Page
     saved_entries = tk.Frame(view_entries_page, bg="#81C046") # this frame contains the saved entries that will be loaded from the JSON file
     saved_entries.grid(row=1, rowspan=7, column=0, columnspan=10, padx=5, pady=5, sticky="nsew")
 
+    entries_rows = 10
+    entries_columns = 1
+
+    for i in range(entries_rows):
+        saved_entries.grid_rowconfigure(i, weight=1)
+    for i in range(entries_columns):
+        saved_entries.grid_columnconfigure(i, weight=1)
+
     home_button = tk.Button(view_entries_page, bg="#245DDA", fg="white", font="Helvitica 18", relief=tk.RAISED, bd=5, text="home", command=lambda: home(view_entries_page)) # home button
     home_button.grid(row=8, column=0, columnspan=10, padx=5, pady=5, sticky="nsew")
 
@@ -107,9 +118,10 @@ def view_entries(): # Page 3 - View Saved Entries Page
 
     # Pack View Saved Entries Page
 
+    display_entries_to_view(saved_entries)
     view_entries_page.pack(fill="both", expand=True)
 
-def delete_entry():
+def delete_entry(): # Page 4 - Delete Entry Page
 
     window.title("aero Text Diary - Delete Entry")
 
@@ -125,6 +137,140 @@ def delete_entry():
     for i in range(pg4columns):
         delete_entry_page.grid_columnconfigure(i, weight=1)
 
+    # Header
+
+    header = tk.Label(delete_entry_page, bg="#245DDA", fg="white", font="Helvitica 40", text="aero text diary")
+    header.grid(row=0, column=0, columnspan=10, padx=10, pady=5, sticky="nsew")
+
+    # Content
+
+    saved_entries = tk.Frame(delete_entry_page, bg="#81C046") # this frame contains the saved entries that will be loaded from the JSON file
+    saved_entries.grid(row=1, rowspan=6, column=0, columnspan=10, padx=5, pady=5, sticky="nsew")
+
+    entries_rows = 10
+    entries_columns = 5
+
+    for i in range(entries_rows):
+        saved_entries.grid_rowconfigure(i, weight=1)
+    for i in range(entries_columns):
+        saved_entries.grid_columnconfigure(i, weight=1)
+
+    delete_label = tk.Label(delete_entry_page, bd=5, bg="yellow", fg="black", font="Helvitica 18", text="enter title to delete") # label instructions for delete textbox
+    delete_label.grid(row=7, column=0, columnspan=5, padx=5, pady=5, sticky="nsew")
+
+    delete_textbox = tk.Entry(delete_entry_page, bg="lightblue", font="Helvitica 18")
+    delete_textbox.grid(row=7, column=5, columnspan=5, padx=5, pady=5, sticky="nsew")
+
+    home_button = tk.Button(delete_entry_page, bg="#245DDA", fg="white", font="Helvitica 18", relief=tk.RAISED, bd=5, text="home", command=lambda: home(delete_entry_page)) # home button
+    home_button.grid(row=8, column=0, columnspan=5, padx=5, pady=5, sticky="nsew")
+
+    submit_button = tk.Button(delete_entry_page, bg="red", fg="white", font="Helvitica 18", relief=tk.RAISED, bd=5, text="delete entry") # submit button
+    submit_button.grid(row=8, column=5, columnspan=5, padx=5, pady=5, sticky="nsew")
+
+    # Footer
+
+    footer = tk.Label(delete_entry_page, bg="#245DDA", fg="white", font="Helvitica 24", text="created by lee gallagher 2025")
+    footer.grid(row=9, column=0, columnspan=10, padx=10, pady=5, sticky="nsew")
+
+    # Pack Delete Entry Page
+
+    display_entries_to_delete(saved_entries)
+    delete_entry_page.pack(fill='both', expand=True)
+
+def help_readme():
+
+    window.title("aero Text Diary - Help & Readme")
+
+    # Grid Configuration
+
+    help_readme_page = tk.Frame(window, bg="#81C046")
+
+    pg5rows = 10
+    pg5columns = 10
+
+    for i in range(pg5rows):
+        help_readme_page.grid_rowconfigure(i, weight=1)
+    for i in range(pg5columns):
+        help_readme_page.grid_columnconfigure(i, weight=1)
+
+    # Header
+
+    header = tk.Label(help_readme_page, bg="#245DDA", fg="white", font="Helvitica 40", text="aero text diary")
+    header.grid(row=0, column=0, columnspan=10, padx=10, pady=5, sticky="nsew")
+
+    # Content
+
+    help_readme_text = tk.Text(help_readme_page, bg="lightblue", font="Helvitica 18", width=52, height=12) # help/readme displayed here
+    help_readme_text.grid(row=1, rowspan=7, column=0, columnspan=8, padx=5, pady=5, sticky="nsew")
+
+    home_button = tk.Button(help_readme_page, bg="#245DDA", fg="white", font="Helvitica 18", relief=tk.RAISED, bd=5, text="home", command=lambda: home(help_readme_page)) # home button
+    home_button.grid(row=8, column=0, columnspan=10, padx=5, pady=5, sticky="nsew")
+    
+    # Footer
+
+    footer = tk.Label(help_readme_page, bg="#245DDA", fg="white", font="Helvitica 24", text="created by lee gallagher 2025")
+    footer.grid(row=9, column=0, columnspan=10, padx=10, pady=5, sticky="nsew")
+
+    # Pack Help & Readme Page
+
+    help_readme_page.pack(fill='both', expand=True)
+
+def read_entry(): # Page 6 - Read Saved Entry Page
+
+    window.title("aero Text Diary - Read Entry")
+
+    # Grid Configuration
+
+    read_entry_page = tk.Frame(window, bg="#81C046")
+
+    pg6rows = 10
+    pg6columns = 10
+
+    for i in range(pg6rows):
+        read_entry_page.grid_rowconfigure(i, weight=1)
+    for i in range(pg6columns):
+        read_entry_page.grid_columnconfigure(i, weight=1)
+
+    # Header
+
+    header = tk.Label(read_entry_page, bg="#245DDA", fg="white", font="Helvitica 40", text="aero text diary")
+    header.grid(row=0, column=0, columnspan=10, padx=10, pady=5, sticky="nsew")
+
+    # Content
+
+    title_label = tk.Label(read_entry_page, bg="#245DDA", fg="white", font="Helvitica 18", text="enter title: ") # title section
+    title_label.grid(row=1, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
+    global entry_title
+    entry_title = tk.Text(read_entry_page, bg="lightblue", font="Helvitica 18", width=45, height=1)
+    entry_title.grid(row=1, column=2, columnspan=8, padx=5, pady=5, sticky="nsew")
+
+    date_label = tk.Label(read_entry_page, bg="#245DDA", fg="white", font="Helvitica 18", text="enter date: ") # date section
+    date_label.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
+    global entry_date
+    entry_date = tk.Text(read_entry_page, bg="lightblue", font="Helvitica 18", width=45, height=1)
+    entry_date.grid(row=2, column=2, columnspan=8, padx=5, pady=5, sticky="nsew")
+
+    main_label = tk.Label(read_entry_page, bg="#245DDA", fg="white", font="Helvitica 18", text="enter article: ") # main article section
+    main_label.grid(row=3, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
+    global entry_main
+    entry_main = tk.Text(read_entry_page, bg="lightblue", font="Helvitica 18", width=45, height=10)
+    entry_main.grid(row=3, rowspan=5, column=2, columnspan=8, padx=5, pady=5, sticky="nsew")
+
+    home_button = tk.Button(read_entry_page, bg="#245DDA", fg="white", font="Helvitica 18", relief=tk.RAISED, bd=5, text="home", command=lambda: home(read_entry_page)) # home button
+    home_button.grid(row=8, column=0, columnspan=10, padx=5, pady=5, sticky="nsew")
+
+    # Footer
+
+    footer = tk.Label(read_entry_page, bg="#245DDA", fg="white", font="Helvitica 24", text="created by lee gallagher 2025")
+    footer.grid(row=9, column=0, columnspan=10, padx=10, pady=5, sticky="nsew")
+
+    # Pack Read Entry Page
+
+    view_entries_page.destroy()
+    read_entry_page.pack(fill='both', expand=True)
+    
+
+
 # ---------- HOME PAGE BUTTON FUNCTIONS ---------- #
 
 def visit_new_entry(home_page):
@@ -137,12 +283,85 @@ def visit_saved_entries(home_page):
     home_page.destroy()
     view_entries()
 
-# ---------- HOME BUTTON(S) ---------- #
+def visit_delete_entry(home_page):
+
+    home_page.destroy()
+    delete_entry()
+
+def visit_help_readme(home_page):
+
+    home_page.destroy()
+    help_readme()
+
+# ---------- HOME BUTTONS FUNCTION ---------- #
 
 def home(x):
 
     x.destroy()
     main()
+
+# ---------- DIARY ENTRY (JSON READ/WRITE) FUNCTIONS ---------- #
+
+def save_new_entry(new_entry_title, new_entry_date, new_entry_main):
+
+    diary_entry = {}
+
+    diary_entry["title"] = new_entry_title.get("1.0", 'end-1c')
+    diary_entry["date"] = new_entry_date.get("1.0", 'end-1c')
+    diary_entry["main"] = new_entry_main.get("1.0", 'end-1c')
+
+    with open (diary_entries_file, 'r') as f:
+        diary_entries = json.load(f)
+
+    diary_entries.append(diary_entry)
+
+    with open (diary_entries_file, 'w') as f:
+        json.dump(diary_entries, f, indent=4)
+
+    new_entry_title.delete(1.0, tk.END)
+    new_entry_date.delete(1.0, tk.END)
+    new_entry_main.delete(1.0, tk.END)
+
+# These two functions form the most difficult part of the program for me, as I struggled to figure out how to make each of the buttons generated in the loop uniquely
+# identifiable to the "load_entry()" function. It turns out that what I was needing was to pass the "diary_entry["title"]" as an argument alongside the "lambda" function and THEN
+# assign that expression to the function itself.
+
+def display_entries_to_view(saved_entries): # displays a button for each entry to allow the user to open a diary entry
+
+    with open (diary_entries_file, 'r') as f: # accesses the JSON file holding the diary entries
+        diary_entries = json.load(f)
+
+    for i, diary_entry in enumerate(diary_entries, start=1):
+        entry_button = tk.Button(saved_entries, bg="#245DDA", fg="white", font="Helvitica 14", text=f"{i}. {diary_entry["title"]}", command=lambda e=diary_entry["title"]: load_entry(e)) # the lambda function that tripped me up!
+        entry_button.grid(row=i-1, columnspan=1, column=0, sticky="nsew", padx=5, pady=5)
+
+def load_entry(entry_selection): # loads the chosen entry onto the "read entry" page
+
+    read_entry()
+
+    with open (diary_entries_file, 'r') as f:
+        diary_entries = json.load(f)
+
+    for diary_entry in diary_entries:
+        if entry_selection == diary_entry["title"]:
+            entry_title.insert("1.0", diary_entry["title"])
+            entry_date.insert("1.0", diary_entry["date"])
+            entry_main.insert("1.0", diary_entry["main"])
+
+# The above was for sure the most frustrating part of the program to build, but very happy with the result - and helped when it came to the delete page as well
+
+def display_entries_to_delete(saved_entries):
+
+    with open (diary_entries_file, 'r') as f: # accesses the JSON file holding the diary entries
+        diary_entries = json.load(f)
+
+    for i, diary_entry in enumerate(diary_entries, start=1):
+
+        delete_entry_label = tk.Label(saved_entries, bg="#245DDA", fg="white", font="Helvitica 14", text=f"{i}. {diary_entry["title"]}")
+        delete_entry_label.grid(row=i-1, columnspan=4, column=0, sticky="nsew", padx=5, pady=5)
+
+        delete_entry_button = tk.Button(saved_entries, bg="red", fg="white", relief=tk.RAISED, bd=5, font="Helvitica 14", text="delete")
+        delete_entry_button.grid(row=i-1, columnspan=1, column=4, sticky="nsew", padx=5, pady=5)
 
 # ---------- PROGRAM START ---------- #
 
@@ -164,7 +383,7 @@ def main(): # Page 1 - Home Page
 
     # Header
 
-    header = tk.Label(home_page, bg="#245DDA", fg="white", font="Helvitica 40", text="aero text diary")
+    header = tk.Label(home_page, bg="#245DDA", fg="white", font="Helvitica 40", text="aero text diary") # Windows XP-style blue
     header.grid(row=0, column=0, columnspan=10, padx=10, pady=5, sticky="nsew")
 
     # Buttons
@@ -175,10 +394,10 @@ def main(): # Page 1 - Home Page
     view_entries_page = tk.Button(home_page, bg="#245DDA", fg="white", font="Helvitica 14", relief=tk.RAISED, bd=5, text="view current entries", command=lambda: visit_saved_entries(home_page))
     view_entries_page.grid(row=1, rowspan=4, column=5, columnspan=5, padx=10, pady=10, sticky="nsew")
 
-    delete_entry_page = tk.Button(home_page, bg="#245DDA", fg="white", font="Helvitica 14", relief=tk.RAISED, bd=5, text="delete saved entry")
+    delete_entry_page = tk.Button(home_page, bg="#245DDA", fg="white", font="Helvitica 14", relief=tk.RAISED, bd=5, text="delete saved entry", command=lambda: visit_delete_entry(home_page))
     delete_entry_page.grid(row=5, rowspan=4, column=0, columnspan=5, padx=10, pady=10, sticky="nsew")
 
-    help_readme_page = tk.Button(home_page, bg="#245DDA", fg="white", font="Helvitica 14", relief=tk.RAISED, bd=5, text="aero text diary")
+    help_readme_page = tk.Button(home_page, bg="#245DDA", fg="white", font="Helvitica 14", relief=tk.RAISED, bd=5, text="help & readme", command=lambda: visit_help_readme(home_page))
     help_readme_page.grid(row=5, rowspan=4, column=5, columnspan=5, padx=10, pady=10, sticky="nsew")
 
     # Footer
